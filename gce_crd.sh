9ac4ae156c5fae45a36062126d2849b65752f98f
makestuffwork <tangbotu@gmail.com>
2018-02-05 18:37:44 +0000

    update gce setup

--- gce_crd.sh
+++ gce_crd.sh
@@ -1,18 +1,26 @@
 #! /bin/bash
 apt-get update
-apt-get install ubuntu-gnome-desktop
-apt-get install vim-gnome
+# apt-get install ubuntu-gnome-desktop
+# apt-get install vim-gnome
+apt-get install xubuntu-desktop
+apt-get install xfce4-goodies
 wget "https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb"
 dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt-get install xubuntu-desktop
 apt-get -f install
-
-# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-# echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-# sudo apt-get update
-# sudo apt-get install google-chrome-stable
 
 # http://remotedesktop.google.com/headless
 # /opt/google/chrome-remote-desktop/start-host --code="4/1_ibBMfignSYHf3f8nzT-UuVG3DKRlM0djXdWNAQgls" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --pin 434755 --name=tlgn
 # /opt/google/chrome-remote-desktop/chrome-remote-desktop --start -s 3840x2160
 
+ssh-keygen -t rsa -b 4096 -C "tangbotu@gmail.com"
+# eval "$(ssh-agent -s)"
+# ssh-add -K ~/.ssh/id_rsa
+
+git clone git@github.com:makestuffwork/my_ubuntu_setup.git
+
+apt-get install vim-gtk
+
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# sudo apt-get update
+# sudo apt-get install google-chrome-stable
